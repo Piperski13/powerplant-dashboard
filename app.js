db1e24b37cda11f12933859774144188bc879980
Aleksa Piperski <mr.alexpiperski@gmail.com>
2024-10-28 14:35:17 +0100

    feat: added folders, cleaning up

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express();
-const db = require('./queries.js')
+const db = require('./controllers/queries.js')
 
 app.use(bodyParser.json())
 app.use(
