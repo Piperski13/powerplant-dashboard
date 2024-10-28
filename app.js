60d5155815cc4cd1c070f5663f7eed1a95a15223
Aleksa Piperski <mr.alexpiperski@gmail.com>
2024-10-28 14:10:05 +0100

    feat: added crud queires, tested with postman, working version

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ app.get('/', (req,res) =>{
   res.json({info: 'NEP API'})
 });
 
-app.get('/records',db);
+app.get('/records',db.getAllRecords);
+app.get('/records/:id',db.getRecord);
+app.post('/records',db.addRecord);
+app.delete('/records/:id',db.deleteRecord);
+app.put('/records/:id',db.updateRecord);
 
 module.exports = app;
--- queries.js
+++ queries.js
@@ -17,4 +17,66 @@ const getAllRecords = (req,res) =>{
    res.status(200).json(results.rows)
   })
  }
- module.exports = getAllRecords;
+ 
+ const getRecord = (req,res) => {
+  const id = parseInt(req.params.id);
+  pool.query('SELECT * FROM evidencijaelektrana WHERE id=$1',[id],(error,results)=>{
+    if (error){
+      throw error;
+    }
+    res.status(200).json(results.rows);
+  })
+ }
+
+ const addRecord = (req,res) => {
+  const {
+    nazivelektrane,
+    mesto,
+    adresa,
+    datumpustanjaurad,
+    sifravrstepogona
+  } = req.body
+
+  pool.query('INSERT INTO evidencijaelektrana (nazivelektrane,mesto,adresa,datumpustanjaurad,sifravrstepogona) VALUES ($1,$2,$3,$4,$5) RETURNING *',[nazivelektrane,mesto,adresa,datumpustanjaurad,sifravrstepogona], (error,results)=>{
+    if (error){
+      throw error;
+    }
+    res.status(201).send(`Record added with ID: ${results.rows[0].id}`)
+  })
+ }
+
+ const deleteRecord = (req,res) =>{
+  const id = parseInt(req.params.id);
+  pool.query('DELETE FROM evidencijaelektrana WHERE id=$1',[id], (error,results)=>{
+    if(error){
+      throw error;
+    }
+    res.status(200).send(`Sucssesfully deleted record with ID:${id}`)
+  })
+ }
+
+ const updateRecord = (req, res) => {
+  const id = parseInt(req.params.id)
+  const {
+    nazivelektrane,
+    mesto,
+    adresa,
+    datumpustanjaurad,
+    sifravrstepogona
+  } = req.body
+
+  pool.query('UPDATE evidencijaelektrana Set nazivelektrane = $1, mesto = $2, adresa = $3, datumpustanjaurad = $4, sifravrstepogona = $5 WHERE id = $6',[nazivelektrane,mesto,adresa,datumpustanjaurad,sifravrstepogona,id], (error,results)=>{
+    if (error){
+      throw error;
+    }
+    res.status(200).send(`Record updated with ID: ${id}`)
+  })
+}
+
+ module.exports = {
+  getAllRecords,
+  getRecord,
+  addRecord,
+  deleteRecord,
+  updateRecord
+ };
