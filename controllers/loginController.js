62295c49893d533277b6194462885442b7dd0c4e
Aleksa Piperski <mr.alexpiperski@gmail.com>
2024-10-31 15:27:12 +0100

    feat: fetching user from database when trying to login

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -3,15 +3,27 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config("../.env");
 
 const getUser = async (username) => {
-  return { userId: 123, password: "ap", username };
+  try {
+    const results = await pool.query(
+      "SELECT * FROM korisnik WHERE korisnickoime=$1",
+      [username]
+    );
+    return results.rows[0];
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 const loginAuth = async (req, res) => {
   const { username, password } = req.body;
 
   const user = await getUser(username);
-
-  if (user.password !== password) {
+  console.log(user);
+  if (!user) {
+    return res.status(403).json({
+      error: "invalid login",
+    });
+  } else if (user.sifra !== password) {
     return res.status(403).json({
       error: "invalid login",
     });
@@ -19,14 +31,16 @@ const loginAuth = async (req, res) => {
 
   delete user.password;
 
-  const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "1s" });
+  const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "1h" });
 
   res.cookie("token", token);
 
   return res.redirect("/View/recordsView.html");
 };
+
 const logout = (req, res) => {
   res.clearCookie("token");
   res.redirect("/");
 };
+
 module.exports = { loginAuth, logout };
