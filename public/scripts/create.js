349822cc36bb594b6741e38fd99e38b072abb320
Aleksa Piperski <mr.alexpiperski@gmail.com>
2024-11-01 12:44:20 +0100

    fix: when bad cookie tries to add record, redirect to login

--- public/scripts/create.js
+++ public/scripts/create.js
@@ -2,7 +2,7 @@ document
   .getElementById("create-form")
   .addEventListener("submit", async (event) => {
     event.preventDefault(); // Prevent default form submission
-    
+
     const formData = new FormData(event.target);
     const formObject = Object.fromEntries(formData.entries());
 
@@ -12,8 +12,17 @@ document
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formObject),
       });
+
+      if (response.ok ) {
+        window.location.href = "View/recordsView.html";
+      }
+      else if (response.status === 401){
+        window.location.href = "/";
+      }
+      else{
+        console.error("Unexpected response:", response.status);
+      }
     } catch (error) {
       console.error(error);
     }
-    window.location.href = "View/recordsView.html";
   });
