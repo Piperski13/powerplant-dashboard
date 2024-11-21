c08dbdd93465fccf5b89d5321c504c761a5f9cae
Aleksa Piperski <mr.alexpiperski@gmail.com>
2024-11-21 17:26:34 +0100

    feat: renamed controller name for view

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const {filterRecords} = require("../controllers/viewController.js");
+const {generateView} = require("../controllers/viewController.js");
 const router = express.Router();
 
-router.route("/").get(filterRecords);
+router.route("/").get(generateView);
 
 module.exports = router;
